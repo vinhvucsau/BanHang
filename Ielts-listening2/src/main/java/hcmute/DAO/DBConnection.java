0f0835c77ff67c917928c89020bea52dc332716e
TranDuongThieu <thieu098dm3@gmail.com>
2023-10-11 09:38:33 +0700

    up Connection

--- Ielts-listening2/src/main/java/hcmute/DAO/DBConnection.java
+++ Ielts-listening2/src/main/java/hcmute/DAO/DBConnection.java
@@ -1,53 +1,31 @@
 package hcmute.DAO;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-import com.mysql.cj.jdbc.Driver;
+public class SqlConnection {
+	private final String serverName = "LAPTOP-6280OKSE\\SQLEXPRESS";
+	private final String dbName = "BanHang";
+	private final String instance = "";
+	private final String userID = "sa";
+	private final String password = "123";
 
-public class DBConnection {
-	private static String DB_URL ="jdbc:mysql://localhost:3306/banhang3";
-	private static String USER_NAME ="root";
-	private static String PASSWORD = "1234567890";
-	private static Connection con ;
-	
-	public static Connection getConnection() throws IOException{
-		con = null ;
-		try {
-			//driver register
-			DriverManager.registerDriver(new Driver());
-			con = (Connection) DriverManager.getConnection(DB_URL,USER_NAME,PASSWORD);
-		} catch (SQLException ex) {
-			Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE,null,ex);
-		}
-		return (con) ;
+	public Connection getConnection() throws Exception {
+		String url = "jdbc:sqlserver://" + serverName  + "\\"  + ";databaseName=" + dbName;
+		if (instance == null || instance.trim().isEmpty())
+			url = "jdbc:sqlserver://" + serverName + ";databaseName=" + dbName;
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		return DriverManager.getConnection(url, userID, password);
 	}
-	
+
+
 	public static void main(String[] args) {
-		/*
-		 * try { Connection c = getConnection();
-		 * System.out.println(DBConnection.getConnection()); if(c== null) {
-		 * System.out.println("something wrong"); } else { System.out.println("ok"); } }
-		 * catch (Exception e) { e.printStackTrace(); }
-		 */
 		try {
-			// connnect to database 'testdb'
-			Connection conn = getConnection();
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from Catetory");
-			while (rs.next()) {
-			System.out.println(rs.getInt(1) + " " + rs.getString(2) 
-			+ " " + rs.getString(3));
-			}
-			conn.close();
-			} catch (Exception ex) {
-			ex.printStackTrace();
-			}
+			System.out.println(new SqlConnection().getConnection());
+						
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
 }
